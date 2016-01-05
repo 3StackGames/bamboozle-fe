448e90f9732ade48df055bb886189a566ccca40e
Jung Oh <Jung3o@yahoo.com>
2016-01-05 00:18:24 -0600

    changed to modal

--- src/components/displayPhases/InitialPhase.js
+++ src/components/displayPhases/InitialPhase.js
@@ -2,11 +2,14 @@ import React, { Component, PropTypes } from 'react'
 import ReactDOM, { render } from 'react-dom'
 import cx from 'classname'
 import { DisplayLobby } from '../../components'
+import Modal from 'react-modal'
 
 export default class InitialPhase extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      displayRejoinModal: false
+    }
   }
 
   componentWillMount() {
@@ -16,21 +19,72 @@ export default class InitialPhase extends Component {
 
     if(gameCode !== null && gameCode.length == 4 && difference <= 3)
     {
-      if(confirm('Found Game Code: ' + gameCode + '\nWould you like to continue?'))
-      {
-        this.props.engine.displayJoin(gameCode)
-        return
-      } else {
-        localStorage.removeItem('display.gameCode')
-        localStorage.removeItem('display.timestamp')
-      }
+      this.setState({
+        displayRejoinModal: true
+      })
+      // if(confirm('Found Game Code: ' + gameCode + '\nWould you like to continue?'))
+      // {
+      //   this.props.engine.displayJoin(gameCode)
+      //   return
+      // } else {
+      //   localStorage.removeItem('display.gameCode')
+      //   localStorage.removeItem('display.timestamp')
+      // }
+    } else {
+      this.props.engine.displayJoin()
     }
-
-    this.props.engine.displayJoin()
   }
 
   render() {
-    const { players = [], gameCode } = this.props.gameState
+    const { gameState, engine } = this.props
+    const { players = [], gameCode } = gameState
+
+    if(this.state.displayRejoinModal) {
+      const gameCode = localStorage.getItem('display.gameCode')
+      const timestamp = parseInt(localStorage.getItem('display.timestamp'), 10)
+      const difference = (+new Date - timestamp) / (1000 * 60 * 60)
+
+      const reJoin = () => {
+        engine.displayJoin(gameCode)
+        this.setState({
+          displayRejoinModal: false
+        })
+      }
+
+      const dontReJoin = () => {
+        engine.displayJoin()
+        this.setState({
+          displayRejoinModal: false
+        })
+      }
+
+      return (
+        <Modal
+          style={{
+            content: {
+              border: null,
+              background: null,
+              overflow: null,
+              borderRadius: null,
+              outline: 0
+            }
+          }}
+          isOpen={true}
+          className="Modal__Bootstrap modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h3>Would you like to continue?</h3>
+              <h4>Found Game Code: <strong>{gameCode}</strong></h4>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-primary btn-modal" onClick={reJoin}>Yes</button>
+              <button className="btn btn-danger btn-modal" onClick={dontReJoin}>No</button>
+            </div>
+          </div>
+        </Modal>
+      )
+    }
+
     return (
       <div>
         <div className="small-header">
